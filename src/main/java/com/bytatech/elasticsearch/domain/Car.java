02842051b12b90138c8251c8618046bd357f13c7
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-09-12 09:07:43 +0530

    add model to domain

--- src/main/java/com/bytatech/elasticsearch/domain/Car.java
+++ src/main/java/com/bytatech/elasticsearch/domain/Car.java
@@ -16,7 +16,7 @@ import java.util.Set;
 public class Car {
 
 	private Long id;
-
+	private Number model;
 	private Integer price;
 
 	private String color;
--- src/main/java/com/bytatech/elasticsearch/service/impl/QueryServiceImpl.java
+++ src/main/java/com/bytatech/elasticsearch/service/impl/QueryServiceImpl.java
@@ -1,4 +1,5 @@
 package com.bytatech.elasticsearch.service.impl;
+
 import static org.elasticsearch.index.query.QueryBuilders.*;
 /*import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -28,93 +29,85 @@ import com.bytatech.elasticsearch.domain.Car;
 import com.bytatech.elasticsearch.domain.User;
 import com.bytatech.elasticsearch.service.QueryService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Service
 public class QueryServiceImpl implements QueryService {
 	private RestHighLevelClient restHighLevelClient;
 
-    private ObjectMapper objectMapper;
+	private ObjectMapper objectMapper;
 
-    public QueryServiceImpl( ObjectMapper objectMapper, RestHighLevelClient restHighLevelClient) {
-        this.objectMapper = objectMapper;
-        this.restHighLevelClient = restHighLevelClient;
-    }
-	public Car getCar(String id){
-		GetRequest getRequest = new GetRequest(
-		        "car", 
-		        id); 
-		  GetResponse getResponse = null;
+	public QueryServiceImpl(ObjectMapper objectMapper, RestHighLevelClient restHighLevelClient) {
+		this.objectMapper = objectMapper;
+		this.restHighLevelClient = restHighLevelClient;
+	}
+
+	public Car getCar(String id) {
+		GetRequest getRequest = new GetRequest("car", id);
+		GetResponse getResponse = null;
 		try {
 			getResponse = restHighLevelClient.get(getRequest, RequestOptions.DEFAULT);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-          Map<String, Object> resultMap = getResponse.getSource();
-          return objectMapper.convertValue(resultMap,Car.class);
+		Map<String, Object> resultMap = getResponse.getSource();
+		return objectMapper.convertValue(resultMap, Car.class);
 
-		
 	}
+
 	public List<Car> findById(Long id) {
-	        List<User> users = new ArrayList<>();
-	        SearchRequest searchRequest = new SearchRequest("car");
-	        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-	        searchSourceBuilder.query(termQuery("id", id));
-	        searchRequest.source(searchSourceBuilder);
-	        
-	        SearchResponse searchResponse=null;
-			try {
-				searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	        return  getSearchResult(searchResponse);
-	
-	        
-	    }
-	
-	  private List<Car> getSearchResult(SearchResponse response) {
+		List<User> users = new ArrayList<>();
+		SearchRequest searchRequest = new SearchRequest("car");
+		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+		searchSourceBuilder.query(termQuery("id", id));
+		searchRequest.source(searchSourceBuilder);
 
-	        SearchHit[] searchHit = response.getHits().getHits();
-
-	        List<Car> carList = new ArrayList<>();
-
-	        for (SearchHit hit : searchHit){
-	            carList
-	                    .add(objectMapper
-	                            .convertValue(hit
-	                                    .getSourceAsMap(), Car.class));
-	        }
-
-	        return carList;
-	    }
-	
-	
-	  @Override
-		public Bucket getCarByColorAggregation() throws IOException {
-			List<String> carList = new ArrayList<String>();
-			 SearchRequest searchRequest = new SearchRequest("car");
-		        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-		        searchSourceBuilder.query(matchAllQuery());
-		        searchSourceBuilder.aggregation(AggregationBuilders.terms("colors").field("color.keyword"));
-		       
-		        searchRequest.source(searchSourceBuilder);
-		        SearchResponse   searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
-		        System.out.println("elasticsearch response: {} totalhitssshits"+ searchResponse.getHits().getTotalHits());
-		        System.out.println("elasticsearch response: {} hits .toostring"+searchResponse.toString());
-		       // searchResponse.getHits().
-		        Aggregations aggregations = searchResponse.getAggregations();
-		        Terms contractSums = searchResponse.getAggregations().get("colors");
-		        for (Terms.Bucket bucket : contractSums.getBuckets()) {
-		        	System.out.println("KEY:"+bucket.getKey()+"!!keyAsString:"+bucket.getKeyAsString()+"!!count:"+bucket.getDocCount());
-		        	
-		        }
-		      
-		        return objectMapper.convertValue(contractSums.getBuckets(),Terms.Bucket.class);
+		SearchResponse searchResponse = null;
+		try {
+			searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	
-	
-	
-	
-	
+		return getSearchResult(searchResponse);
+
+	}
+
+	private List<Car> getSearchResult(SearchResponse response) {
+
+		SearchHit[] searchHit = response.getHits().getHits();
+
+		List<Car> carList = new ArrayList<>();
+
+		for (SearchHit hit : searchHit) {
+			carList.add(objectMapper.convertValue(hit.getSourceAsMap(), Car.class));
+		}
+
+		return carList;
+	}
+
+	@Override
+	public Bucket getCarByColorAggregation() throws IOException {
+		List<String> carList = new ArrayList<String>();
+		SearchRequest searchRequest = new SearchRequest("car");
+		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+		searchSourceBuilder.query(matchAllQuery());
+		searchSourceBuilder.aggregation(AggregationBuilders.terms("models").field("model"));
+
+		searchRequest.source(searchSourceBuilder);
+		SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+		System.out.println("elasticsearch response: {} totalhitssshits" + searchResponse.getHits().getTotalHits());
+		System.out.println("elasticsearch response: {} hits .toostring" + searchResponse.toString());
+		// searchResponse.getHits().
+		Aggregations aggregations = searchResponse.getAggregations();
+		Terms contractSums = searchResponse.getAggregations().get("models");
+		for (Terms.Bucket bucket : contractSums.getBuckets()) {
+			System.out.println("KEY:" + bucket.getKey() + "!!keyAsString:" + bucket.getKeyAsString() + "!!count:"
+					+ bucket.getDocCount());
+
+		}
+
+		return objectMapper.convertValue(contractSums.getBuckets(), Terms.Bucket.class);
+	}
+
 }
